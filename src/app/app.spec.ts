73992c72924c73c1a0faac3c0216f0a1bfecf961
Levin <L.ostertag.inf22@stud.bbbaden.ch>
2025-10-14 16:00:49 +0200

    Fix App Tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,17 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([]), HttpClientTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         App
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
